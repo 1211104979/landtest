6bd70f993ba99b375692e201d2d1f3c7c7c6225f
1211104979 <1211104979@student.mmu.edu.my>
2025-06-17 21:13:10 +0800

    update

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -6,10 +6,6 @@ import About from './pages/about';
 import Service from './pages/service';
 import Login from './pages/login';
 import Register from './pages/register';
-import UsTransactions from './pages/user/transaction';
-import UsProperties from './pages/user/property';
-import RegisterLandPage from './pages/user/registerLand';
-import Test from './pages/user/test';
 
 // AuthProvider をインポート
 import { AuthProvider } from './AuthContext';
@@ -24,10 +20,6 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/about" element={<About />} />
         <Route path="/service" element={<Service />} />
-        <Route path="/user/properties" element={<UsProperties />} />
-        <Route path="/user/transactions" element={<UsTransactions />} />
-        <Route path="/user/register-land" element={<RegisterLandPage />} />
-        <Route path="/user/test" element={<Test />} />
       </Routes>
     </AuthProvider>
   );
